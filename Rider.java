b44c8cb9813c54a46fcb87e2f5820d80d9b1080b
Kushan <ra.kushan1997@gmail.com>
2021-09-24 16:55:49 +0530

    first commit

---
+++ Rider.java
@@ -0,0 +1,43 @@
+import java.util.concurrent.*;
+class Rider extends Thread {
+    Semaphore mutex;
+    Semaphore bus;
+    Semaphore allAboard;
+    Semaphore multiplex;
+    String thread_name;
+
+    public Rider(Semaphore mutex, Semaphore bus, Semaphore allAboard, Semaphore multiplex,
+            String thread_name) {
+        super(thread_name);
+        this.mutex = mutex;
+        this.bus = bus;
+        this.allAboard = allAboard;
+        this.multiplex = multiplex;
+        this.thread_name = thread_name;
+    }
+
+    @Override
+    public void run() {
+        try {
+            this.multiplex.acquire();
+            this.mutex.acquire();
+            SemaphoreDemo.riders += 1;
+            this.mutex.release();
+            this.bus.acquire();
+            this.multiplex.release();
+            this.boardBus();
+            SemaphoreDemo.riders -= 1;
+            if (SemaphoreDemo.riders == 0) {
+                this.allAboard.release();
+            } else {
+                this.bus.release();
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void boardBus() {
+        System.out.println("Board Bus " + thread_name + "...............");
+    }
+}
